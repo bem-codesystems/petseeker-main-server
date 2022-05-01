967057a19d54d0872d365c5d557942c04c35e10a
adrianohellermylla <adrianohellermylla@gmail.com>
2022-04-30 22:15:36 -0300

    feat: Update User constructor

--- src/models/User.ts
+++ src/models/User.ts
@@ -4,12 +4,14 @@ class User {
     email: string;
     createdAt: number;
     hasWallet: boolean;
-   constructor(id: string,name: string,email: string,createdAt: number,hasWallet: boolean){
+    updatedAt: number;
+   constructor(id: string,name: string,email: string,createdAt: number,hasWallet: boolean, updatedAt: number){
        this.id = id;
        this.name = name;
        this.email = email;
        this.createdAt = createdAt;
        this.hasWallet = hasWallet;
+       this.updatedAt = updatedAt;
    };
 }
 
